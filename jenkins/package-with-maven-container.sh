9362e8d578e93e8f2b48ed2fe13bc4f8d8a5a4ae
HIANDIC <leaands25@gmail.com>
2023-02-15 10:21:37 +0000

    added jenkinsfile petclinic-production for main branch

--- jenkins/package-with-maven-container.sh
+++ jenkins/package-with-maven-container.sh
@@ -1,1 +1,7 @@
+# jar file elde etmek icin app imizi package liyoruz (paketleme yapiyoruz)
+# microservice lerin image larindan paketleme islemi yapiyoruz
+# burada container olusturuyoruz ve olusan container da <mvn clean package> komutunu calistiriyoruz. bu komut calisanca da target klasörü altinda artifact leri olusturuyor (jar file, vb.)
+# project imin root dizinini (diger bir ifade ile projemin ana klasörünü/proje nin bütün klasör ve file larini) container icerisindeki app folder ina mount ediyoruz.
+# -w /app flag i ile de pom.xml file ini bulabilmesi icin /app folder ini working directory olarak tanimliyoruz
+# maven:3.6-openjdk-11 image indan container i olusturuyoruz. Bu sekilde local host umuza/makinemize maven yüklemeden container icerisinde mvn komutunu calistirabildik
 docker run --rm -v $HOME/.m2:/root/.m2 -v $WORKSPACE:/app -w /app maven:3.6-openjdk-11 mvn clean package
